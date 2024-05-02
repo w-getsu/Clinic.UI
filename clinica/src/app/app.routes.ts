3b69bfd11a446af8fcd683350b05a4a15bd330c5
Akmalxo'ja <145533924+akmalxoja@users.noreply.github.com>
2024-05-02 20:11:48 +0500

    doctor

--- clinica/src/app/app.routes.ts
+++ clinica/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/identity-component/login/login.component';
 import { RegisterComponent } from './components/identity-component/register/register.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { DoctorComponent } from './components/pages/doctor/doctor.component';
 
 export const routes: Routes = [
     {path : 'login', component : LoginComponent},
     {path: 'register', component : RegisterComponent},
-    {path: 'home',component:HomeComponent}
+    {path: 'home',component:HomeComponent},
+    {path: 'doctor', component: DoctorComponent}
 ];
