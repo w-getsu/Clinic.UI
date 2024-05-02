a625106dbef5eddaf6a0d679536adbd0f77567d4
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-02 15:50:38 +0500

    Add Router Link

--- clinica/src/app/components/identity-component/register/register.component.ts
+++ clinica/src/app/components/identity-component/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  constructor(private router : Router){}
 
+  toLogin()
+  {
+    this.router.navigateByUrl('/login')
+  }
 }
