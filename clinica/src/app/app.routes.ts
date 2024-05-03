0e526d0026e78815cc824efe08145fdbd15ec815
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-03 08:09:06 +0500

    Add not found page

--- clinica/src/app/app.routes.ts
+++ clinica/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/identity-component/login/login.component';
 import { RegisterComponent } from './components/identity-component/register/register.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {path : 'login', component : LoginComponent},
     {path: 'register', component : RegisterComponent},
-    {path: 'home',component:HomeComponent}
+    {path: 'home',component:HomeComponent},
+    {path:'',component:HomeComponent},
+    {path:'**',component:NotFoundComponent}
 ];
--- clinica/src/app/components/pages/home/home.component.ts
+++ clinica/src/app/components/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../static-pages/navbar/navbar.component';
+import { FooterComponent } from '../../static-pages/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
