cf8229c1ede7cebdd30ecb89831c9574bf7e1078
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-05-04 17:23:30 +0500

    home 4

--- clinica/src/app/app.routes.ts
+++ clinica/src/app/app.routes.ts
@@ -9,13 +9,14 @@ import { adminGuard,clinicGuard,expireGuard } from './guards/clinic.guard';
 import { DashboardComponent } from './components/admin-panel/dashboard/dashboard.component';
 import { SpecialistsComponent } from './components/pages/specialists/specialists.component';
 import { SkillsComponent } from './components/admin-panel/skills/skills.component';
-import { ServiceComponent } from './components/pages/service/service.component';
 import { ServicesComponent } from './components/pages/services/services.component';
 import { PricingComponent } from './components/pages/pricing/pricing.component';
 import { FeedbackComponent } from './components/pages/feedback/feedback.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { UpdateComponent } from './components/admin-panel/skills/update/update.component';
 import { HomeDashComponent } from './components/admin-panel/home-dash/home-dash.component';
+import { ServiceComponent } from './components/pages/service/service.component';
+import { ContactsComponent } from './components/pages/contacts/contacts.component';
 
 export const routes: Routes = [
     {path : 'login', component : LoginComponent},
--- clinica/src/app/components/pages/home/home.component.ts
+++ clinica/src/app/components/pages/home/home.component.ts
@@ -2,14 +2,35 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../static-pages/navbar/navbar.component';
 import { FooterComponent } from '../../static-pages/footer/footer.component';
 import { DurationTimeComponent } from '../../static-pages/duration-time/duration-time.component';
+import { Specialists } from '../../../interfaces/specialists';
+import { SpecialistsService } from '../../../services/specialists.service';
+import { CardComponent } from '../../static-pages/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,DurationTimeComponent],
+  imports: [NavbarComponent,FooterComponent,DurationTimeComponent,CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  Doctors!:Specialists[]
+  pageIndex:number=1
+  size:number=10
+  constructor(private specialistsService:SpecialistsService){
+    this.getall()
+  }
+  getall(){
+    this.specialistsService.getAllDoctors(this.pageIndex,this.size).subscribe({
+      next:(data)=>{
+        this.Doctors=data
+        console.log(data)
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    })
+  }
+
 
 }
