a1400ea69186f90f09520236519a8b20f919885f
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-06 14:31:43 +0500

    some chnages

--- clinica/src/app/components/pages/about/about.component.ts
+++ clinica/src/app/components/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../static-pages/navbar/navbar.component';
+import { FooterComponent } from '../../static-pages/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- clinica/src/app/components/static-pages/navbar/navbar.component.ts
+++ clinica/src/app/components/static-pages/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router,) {}
 
   isRegisterPage(): boolean {
     return this.router.url ==='/register';
