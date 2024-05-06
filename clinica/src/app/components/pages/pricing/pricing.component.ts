146b6264576895a501eae50c255125f308c8c1ab
Akmalxo'ja <145533924+akmalxoja@users.noreply.github.com>
2024-05-06 20:02:51 +0500

    sm

--- clinica/src/app/components/pages/pricing/pricing.component.ts
+++ clinica/src/app/components/pages/pricing/pricing.component.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../static-pages/navbar/navbar.component';
 import { FooterComponent } from '../../static-pages/footer/footer.component';
-<<<<<<< HEAD
 import { MiniSidebarComponent } from '../../static-pages/mini-sidebar/mini-sidebar.component';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputMaskModule } from 'primeng/inputmask';
 import { FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
-=======
->>>>>>> 9ba3fa64c4d61364217adcf611538ee923ab82f5
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-<<<<<<< HEAD
   imports: [
     NavbarComponent,
     InputMaskModule,
@@ -25,12 +19,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     FooterComponent,
     MiniSidebarComponent,
     FormsModule,
-     MatFormFieldModule, 
-     MatInputModule
+     NavbarComponent,
   ],
-=======
-  imports: [NavbarComponent,FooterComponent],
->>>>>>> 9ba3fa64c4d61364217adcf611538ee923ab82f5
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.scss'
 })
